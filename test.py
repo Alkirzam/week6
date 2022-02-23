ae1399ae3d29c47726b603fcf84f0b1b8e0a3c04
alialkirzam@hotmail.com <alialkirzam@hotmail.com>
2022-02-23 17:56:58 +0000

    tes

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-print("team 7 members are: ......")
+#import pandas as pd
+#data = pd.read_csv("C:/Users/alialkirzam/OneDrive - Liverpool John Moores University/Desktop/computer scince workshop/dataset/rotten_tomatoes_movies.csv")
+
+#x = data["genres"]
+#y=data["content_rating"]
+
+#print(x,y)
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+data = pd.read_excel("C:/Users/alialkirzam/OneDrive - Liverpool John Moores University/Desktop/computer scince workshop/dataset/rotten_tomatoes_movies_2.xlsx")
+print(data)
+pivot=data.groupby(['Fresh']).mean()
+fresh_movie=pivot.loc[:,"movie_title(Mine)":"tomatometer_status(Units)"]
+fresh_movie.plot(kind='bar')
+plt.show()
