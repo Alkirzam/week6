250fd9f296781e3445f98d416fd929575b598d11
alialkirzam@hotmail.com <alialkirzam@hotmail.com>
2022-02-27 14:04:32 +0000

    n

---
+++ test2.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+data = pd.read_excel("C:/Users/alialkirzam/OneDrive - Liverpool John Moores University/Desktop/computer scince workshop/dataset/rotten_tomatoes_movies_2.xlsx")
+data.head()
+
+#plt.plot(data. movie_title content_rating)
+
+#path =r"C:/Users/alialkirzam/OneDrive - Liverpool John Moores University/Desktop/computer scince workshop/dataset/rotten_tomatoes_movies_2.xlsx"
+#df=pd.read_excel(path,0, engine='openpyxl')
+#df.head()
